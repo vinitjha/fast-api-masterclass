928b9156c2c85b35a20e866ddeb51d344716e318
vinitjha <vinitjha.aec@gmail.com>
2026-09-13 08:13:58 -0700

    Update main.py

--- rent-a-room/main.py
+++ rent-a-room/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status
 
 app = FastAPI()
-@app.get("/")
+@app.get("/",status_code=status.HTTP_103_EARLY_HINTS)
 def root():
    return {"message": "Welcome to Rent a rooms"}
